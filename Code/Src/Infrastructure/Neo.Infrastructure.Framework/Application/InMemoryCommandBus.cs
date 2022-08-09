72bb1b060c83c1e4786c479428015c62ea7b9d83
MohammadReza <mohammadtaghipoor@yahoo.com>
2022-08-09 12:20:55 +0430

    add missing nuget packages

--- Code/Src/Infrastructure/Neo.Infrastructure.Framework/Application/InMemoryCommandBus.cs
+++ Code/Src/Infrastructure/Neo.Infrastructure.Framework/Application/InMemoryCommandBus.cs
@@ -1,21 +1,23 @@
+using Microsoft.Extensions.Logging;
+
 namespace Neo.Infrastructure.Framework.Application;
 
 public class InMemoryCommandBus : ICommandBus
 {
     readonly IServiceProvider _serviceProvider;
-    // readonly ILogger<InMemoryCommandBus> _logger;
+    readonly ILogger<InMemoryCommandBus> _logger;
 
-    public InMemoryCommandBus(IServiceProvider serviceProvider
-    // ,ILogger<InMemoryCommandBus> logger
+    public InMemoryCommandBus(IServiceProvider serviceProvider,
+        ILogger<InMemoryCommandBus> logger
     )
     {
         _serviceProvider = serviceProvider;
-        // _logger = logger;
+        _logger = logger;
     }
 
     public async Task Dispatch<T>(T command) where T : ICommand
     {
-        // _logger.LogInformation($"Starting handling command of type: {command.GetType().Name}.");
+        _logger.LogInformation($"Starting handling command of type: {command.GetType().Name}.");
 
         var handler = _serviceProvider
             .GetService(typeof(ICommandHandler<T>)) as ICommandHandler<T>;
@@ -25,6 +27,6 @@ public class InMemoryCommandBus : ICommandBus
 
         await handler.Handle(command).ConfigureAwait(false);
 
-        // _logger.LogInformation($"Handling command of type: {command.GetType().Name} finished successfully");
+        _logger.LogInformation($"Handling command of type: {command.GetType().Name} finished successfully");
     }
 }
