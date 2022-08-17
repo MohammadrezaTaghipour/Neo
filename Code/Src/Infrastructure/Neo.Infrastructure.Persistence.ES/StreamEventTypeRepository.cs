5a3b4c2d520014694e4aafc69224cb4300392891
MohammadReza <mohammadtaghipoor@yahoo.com>
2022-08-17 17:38:46 +0430

    add new project Neo.Infrastructure.Persistence.ES

---
+++ Code/Src/Infrastructure/Neo.Infrastructure.Persistence.ES/StreamEventTypeRepository.cs
@@ -0,0 +1,22 @@
+﻿using Neo.Domain.Contracts.StreamEventTypes;
+using Neo.Domain.Models.StreamEventTypes;
+
+namespace Neo.Infrastructure.Persistence.ES;
+
+public class StreamEventTypeRepository : IStreamEventTypeRepository
+{
+    public Task<StreamEventType> GetBy(StreamEventTypeId id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<StreamEventType> GetBy(StreamEventTypeId id, int version)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Add(StreamEventType streamEventType)
+    {
+        throw new NotImplementedException();
+    }
+}
