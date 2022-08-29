f751580d7bd6e053749e3f045ce31cb95de189f1
MohammadReza <mohammadtaghipoor@yahoo.com>
2022-08-29 17:05:16 +0430

    add StreamEventTypeRepository

--- Code/Src/Infrastructure/Neo.Infrastructure.Persistence.ES/StreamEventTypeRepository.cs
+++ Code/Src/Infrastructure/Neo.Infrastructure.Persistence.ES/StreamEventTypeRepository.cs
@@ -1,25 +1,31 @@
 ﻿using Neo.Domain.Contracts.StreamEventTypes;
 using Neo.Domain.Models.StreamEventTypes;
+using Neo.Infrastructure.Framework.Persistence;
 
 namespace Neo.Infrastructure.Persistence.ES;
 
 public class StreamEventTypeRepository : IStreamEventTypeRepository
 {
-    public Task<StreamEventType> GetBy(StreamEventTypeId id,
-        CancellationToken cancellationToken)
+    private IEventSourcedRepository<StreamEventType, StreamEventTypeState, StreamEventTypeId> _repository;
+
+    public StreamEventTypeRepository(
+        IEventSourcedRepository<StreamEventType, StreamEventTypeState, StreamEventTypeId> repository)
     {
-        throw new NotImplementedException();
+        _repository = repository;
     }
 
-    public Task<StreamEventType> GetBy(StreamEventTypeId id, int version,
+    public async Task<StreamEventType> GetBy(StreamEventTypeId id,
         CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+        => await _repository.GetById(id, cancellationToken)
+            .ConfigureAwait(false);
 
-    public Task Add(StreamEventType streamEventType,
+    public async Task<StreamEventType> GetBy(StreamEventTypeId id,
+        int version, CancellationToken cancellationToken)
+        => await _repository.GetBy(id, version, cancellationToken)
+            .ConfigureAwait(false);
+
+    public async Task Add(StreamEventType streamEventType,
         CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+        => await _repository.Add(streamEventType, cancellationToken)
+            .ConfigureAwait(false);
 }
