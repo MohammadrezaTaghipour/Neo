147c1f2aa13d8cce3d58100d2e3c14866b9e801b
MohammadReza <mohammadtaghipoor@yahoo.com>
2022-09-05 17:21:57 +0430

    refactor EsdbRepository

--- Code/Src/ServiceHost/Configurations/NeoBootstrapper.cs
+++ Code/Src/ServiceHost/Configurations/NeoBootstrapper.cs
@@ -3,6 +3,7 @@ using Neo.Application.StreamEventTypes;
 using Neo.Infrastructure.Framework.Application;
 using Neo.Infrastructure.Framework.Configurations;
 using Neo.Infrastructure.Framework.Domain;
+using Neo.Infrastructure.Persistence.ES;
 
 namespace ServiceHost.Configurations;
 
@@ -10,6 +11,7 @@ public class NeoBootstrapper : IBootstrapper
 {
     public void Bootstrap(IServiceCollection services)
     {
+        AddDomainRepository(services, typeof(StreamEventTypeRepository).Assembly);
         AddApplicationServices(services, typeof(StreamEventTypeService).Assembly);
         AddArgFactories(services, typeof(StreamEventTypeArgFactory).Assembly);
     }
@@ -38,21 +40,35 @@ public class NeoBootstrapper : IBootstrapper
     static void AddArgFactories(IServiceCollection services,
         Assembly assembly)
     {
+        
         assembly
             .GetTypes()
-            .Where(item => item.GetInterfaces()
-                               .Where(i => i.IsGenericType)
-                               .Any(i => i.GetGenericTypeDefinition() ==
-                                         typeof(IDomainArgFactory))
-                           && !item.IsAbstract && !item.IsInterface)
+            .Where(a => a.Name.EndsWith("ArgFactory") && !a.IsAbstract && !a.IsInterface)
+            .Select(a => new { assignedType = a, serviceTypes = a.GetInterfaces().ToList() })
             .ToList()
-            .ForEach(assignedTypes =>
+            .ForEach(typesToRegister =>
             {
-                var serviceType = assignedTypes
-                    .GetInterfaces()
-                    .First(i => i.GetGenericTypeDefinition() ==
-                                typeof(IDomainArgFactory));
-                services.AddScoped(serviceType, assignedTypes);
+                typesToRegister.serviceTypes
+                    .ForEach(typeToRegister => services
+                        .AddScoped(typeToRegister, typesToRegister.assignedType));
             });
+
+    }
+
+    static void AddDomainRepository(IServiceCollection services,
+        Assembly assembly)
+    {
+        assembly
+            .GetTypes()
+            .Where(a => a.Name.EndsWith("Repository") && !a.IsAbstract && !a.IsInterface)
+            .Select(a => new { assignedType = a, serviceTypes = a.GetInterfaces().ToList() })
+            .ToList()
+            .ForEach(typesToRegister =>
+            {
+                typesToRegister.serviceTypes
+                    .ForEach(typeToRegister => services
+                        .AddScoped(typeToRegister, typesToRegister.assignedType));
+            });
+        
     }
 }
